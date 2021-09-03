f130e4555d16c880706dbba102cb4ff1c650c010
czyjtu <maciejczyjtu@gmail.com>
2021-09-03 17:38:58 +0200

    Start screen/endgame screen added

--- app/GUI/board.py
+++ app/GUI/board.py
@@ -4,12 +4,15 @@ from game import Connect4
 import pygame
 from pygame import gfxdraw
 import pickle   
+import numpy as np
+from scipy.signal import convolve2d
+
 
 
 class Board:
     CHIP_RADIUS = 0.4 # 90% of the column width
     CHIP_OFFSET = 0.1 # 10% of the column width
-    PLAYER_COL = {1: (82, 212, 212) , -1: (255, 195, 0 ), 0: (157, 168, 187)}
+    PLAYER_COL = {1: (215, 75, 75) , -1: (255, 195, 0 ), 0: (157, 168, 187)}
 
 
     def __init__(self, screen_size):
@@ -22,6 +25,14 @@ class Board:
         with open("data/8ply.pkl", "rb") as f:
             data = pickle.load(f)
         self.ai_player = lambda *args: idminmax_player(*args, lookup_table=data)
+        self.ply_num = 0
+        
+        horizontal_kernel = np.array([[ 1, 1, 1, 1]])
+        vertical_kernel = np.transpose(horizontal_kernel)
+        diag1_kernel = np.eye(4, dtype=np.uint8)
+        diag2_kernel = np.fliplr(diag1_kernel)
+        self.detection_kernels = [horizontal_kernel, vertical_kernel, diag1_kernel, diag2_kernel]
+        self.kernel_direct = [(0, 1), (1, 0), (1, 1), (-1, 1)]
 
 
     def update(self):
@@ -30,22 +41,37 @@ class Board:
         self._ply(a)
 
 
-    def render(self, screen):
+    def _render_chip(self, screen, row, column, colour):
+        gfxdraw.aacircle(
+            screen,
+            int(0.5*self.COL_WIDTH + column*self.COL_WIDTH), int(1.5*self.ROW_HEIGHT + row*self.ROW_HEIGHT), 
+            int(Board.CHIP_RADIUS*self.COL_WIDTH),  colour
+        )
+        gfxdraw.filled_circle(
+            screen, 
+            int(0.5*self.COL_WIDTH + column*self.COL_WIDTH), int(1.5*self.ROW_HEIGHT + row*self.ROW_HEIGHT), 
+            int(Board.CHIP_RADIUS*self.COL_WIDTH), colour
+        )
+
+
+    def _render_board(self, screen, winning_chips=set()):
         for row, spots in enumerate(self.state.as_board()):
             for column, spot in enumerate(spots):
-                gfxdraw.aacircle(
-                    screen,
-                    int(0.5*self.COL_WIDTH + column*self.COL_WIDTH), int(1.5*self.ROW_HEIGHT + row*self.ROW_HEIGHT), 
-                    int(Board.CHIP_RADIUS*self.COL_WIDTH),  Board.PLAYER_COL[spot]
-                )
-                gfxdraw.filled_circle(
-                    screen, 
-                    int(0.5*self.COL_WIDTH + column*self.COL_WIDTH), int(1.5*self.ROW_HEIGHT + row*self.ROW_HEIGHT), 
-                    int(Board.CHIP_RADIUS*self.COL_WIDTH),  Board.PLAYER_COL[spot]
-                )
+                alpha = 255 if (len(winning_chips) == 0 or (row, column) in winning_chips) else 100
+                self._render_chip(screen, row, column, (*Board.PLAYER_COL[spot], alpha))
 
 
-    def get_column(self, posx):
+    def render_endgame(self, screen):
+        result = {1: "PLAYER 1 WON", 0: "DRAW", -1: "PLAYER 2 WON"}
+        winning_chips = self._get_winning_chips()
+        self._render_board(screen, winning_chips)
+
+
+    def render_game(self, screen):
+        self._render_board(screen)
+
+
+    def get_column_from_pos(self, posx):
         print(posx, self.COL_WIDTH)
         col = int(posx // self.COL_WIDTH)
         print(f"player move: {col}")
@@ -60,22 +86,26 @@ class Board:
         if col in self.game.actions(self.state):
             self._ply(col)
 
-
-    def _render_end_game(self, screen):
-        result = {
-            1: "PLAYER 1 WON",
-            0: "DRAW",
-            -1: "PLAYER 2 WON"
-        }
-        pygame.font.init()
-        myfont = pygame.font.SysFont('Comic Sans MS', 30)
-        
-        textsurface = myfont.render(f'GAME OVER: {result[self.state.utility]}', False, (0, 0, 0))
-        screen.blit(textsurface,(0,0))
+    
+    def _get_winning_chips(self):
+        board = self.state.as_board()
+        for player in [-1, 1]:
+            for i, kernel in enumerate(self.detection_kernels):
+                conv = convolve2d(board == player, kernel, mode="valid")
+                if (conv== 4).any():
+                    where_row, where_col = np.where(conv == 4)
+                    r, c = where_row[0], where_col[0]
+                    if i == 3: #poositive diagonal treated differenty
+                        r += 4 # left bottom corner of the kernel
+                    dir = self.kernel_direct[i]
+                    idxs = [(r + j*dir[0], c + j*dir[1]) for j in range(4)]
+                    return set(idxs)
+        return set()
 
 
     def _ply(self, col):
         if not self.finished:
+            self.ply_num += 1
             self.game.make_move(self.state, col)
         if self.game.terminal_test(self.state):
             u = self.state.utility
--- app/GUI/session.py
+++ app/GUI/session.py
@@ -1,46 +1,66 @@
 from game.connect4 import Connect4
 from GUI.board import Board
+from GUI.button import Button
 import pygame
+import enum 
+pygame.font.init()
 
 
 class Session:
-    SCREEN_SIZE = (720, 720)
+    SCREEN_SIZE = (512, 512)
     BACKGROUND_COL = (82, 92, 108)
+    FONT = pygame.font.Font(None,30)
+    FONT_COL = '#FFFFFF'
+    mode = enum.Enum('Mode', 'START, GAME, ENDGAME')
 
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(Session.SCREEN_SIZE)
+        self.grid_w = Session.SCREEN_SIZE[0] // 8
+        self.grid_h = Session.SCREEN_SIZE[1] // 10
+        b_w = 2 * self.grid_w
+        b_h = self.grid_h
+        self.button_player = Button('Me first', b_w, b_h, (self.grid_w, 2*self.grid_h), 5)
+        self.button_ai = Button('AI first', b_w, b_h, (5*self.grid_w, 2*self.grid_h), 5)
+        self.running = True
         self.new_game()
 
 
     def new_game(self):
+        self.mode = Session.mode.START
         self.board = Board(Session.SCREEN_SIZE)
-        self.ai_player_id = 1
+        self.ai_player_id = None # clik to set it up
         self.clock = pygame.time.Clock()
-        self.running = True
 
 
     def render(self):
-        if self.board.finished:
+        self.screen.fill(Session.BACKGROUND_COL)
+        if self.mode == Session.mode.START:
+            self._start_screen()
+        elif self.mode == Session.mode.ENDGAME:
             self._end_screen()
         else:
             self._game_screen()
+        pygame.display.flip()
 
 
     def _start_screen(self):
-        pass
-
+        textsurface = Session.FONT.render(f"WHO'S FIRST?", True, Session.FONT_COL)
+        self.screen.blit(textsurface,(3*self.grid_w, self.grid_h))
+        self.button_player.draw(self.screen)
+        self.button_ai.draw(self.screen)
+          
 
     def _game_screen(self):
-        self.screen.fill(Session.BACKGROUND_COL)
-        self.board.render(self.screen)
-        pygame.display.flip()
+        self.board.render_game(self.screen)
 
 
     def _end_screen(self):
-        self.screen.fill(Session.BACKGROUND_COL)
-        self.board._render_end_game(self.screen)
-        pygame.display.flip()
+        textsurface = Session.FONT.render(f'GAME OVER', True, Session.FONT_COL)
+        self.screen.blit(textsurface, (3*self.grid_w, self.grid_h // 4))
+        textsurface = Session.FONT.render(f"PRESS 'R' TO RESTART", True, Session.FONT_COL)
+        self.screen.blit(textsurface, (int(2.2*self.grid_w), self.grid_h))
+        self.board.render_endgame(self.screen)
 
 
     def update(self):
@@ -52,20 +72,48 @@ class Session:
         self.clock.tick(fps)
     
 
-    def handle_events(self, navigation=True):
+    def handle_events(self):
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    self.new_game()
             if event.type == pygame.QUIT:
                 self.running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                posx = event.pos[0]
-                if self.board.get_turn() != self.ai_player_id:
-                    col = self.board.get_column(posx)
-                    self.board.drop_chip(col)
+            if self.mode == Session.mode.START:
+                self._start_events(event)
+            elif self.mode == Session.mode.GAME:
+                self._game_events(event)
+            elif self.mode == Session.mode.ENDGAME:
+                self._endgame_events(event)
+
+
+    def _start_events(self, event):
+        pressed = False
+        if self.button_player.pressed:
+            self.button_player.pressed = False
+            pressed = True
+            self.ai_player_id = 1
+        elif self.button_ai.pressed:
+            self.button_ai.pressed = False
+            pressed = True
+            self.ai_player_id = 0
+        self.mode = Session.mode.GAME if pressed else self.mode
+
+    
+    def _game_events(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                self.new_game()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            posx = event.pos[0]
+            if self.board.get_turn() != self.ai_player_id:
+                col = self.board.get_column_from_pos(posx)
+                self.board.drop_chip(col)
+        self.mode = Session.mode.ENDGAME if self.board.finished else self.mode
+
+
+    def _endgame_events(self, event):
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+            self.new_game()
 
 
     def cleanup(self):
-        # display 
+        self.running = False
         pygame.quit()
